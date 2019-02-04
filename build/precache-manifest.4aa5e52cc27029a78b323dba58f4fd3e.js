9f0b8b76b8f08301e9486e123d31bf676b6ef5c2
David Hildering <david@purplebean.nl>
2019-02-04 14:48:47 +0100

    add node express server

---
+++ build/precache-manifest.4aa5e52cc27029a78b323dba58f4fd3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "db5e61f612f5e3867ec7",
+    "url": "/static/js/main.db5e61f6.chunk.js"
+  },
+  {
+    "revision": "eaf0d64d93b2d2f2ba6f",
+    "url": "/static/js/1.eaf0d64d.chunk.js"
+  },
+  {
+    "revision": "db5e61f612f5e3867ec7",
+    "url": "/static/css/main.04d69452.chunk.css"
+  },
+  {
+    "revision": "100327ec9c46bcf71636f00b5e9cedd8",
+    "url": "/index.html"
+  }
+];
